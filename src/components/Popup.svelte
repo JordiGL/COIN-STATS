28d77993040df940bbab24584ef1a0d65b94c468
JordiGL <jordi.g.l@live.com>
2021-06-24 19:23:26 +0200

    prPopupBorrat

--- src/components/Popup.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  export let moneda = [];
-  import Popup from "svelte-atoms/Popup.svelte";
-  export let isOpen = false;
-  const close = () => (isOpen = false);
-</script>
-
-<Popup {isOpen} on:close={close} header={moneda.symbol}>{moneda.volume}</Popup>
