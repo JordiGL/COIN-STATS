f636eac5b84e158a62520c9ba9135c09017103c3
JordiGL <jordi.g.l@live.com>
2021-07-05 16:30:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import PostList from './components/PostList';
 function App() {
   return (
     <div className="App">
-      <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"></link>
-      <Header />
+      {/* <Header /> */}
       <PostList />
     </div>
   );
